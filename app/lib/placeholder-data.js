90fb202afb01d19d23b7b4889d0ec2ae4200da8f
zhanglei <01439283>
2024-12-09 15:52:10 +0800

    增加mock数据

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -7,6 +7,12 @@ const users = [
     email: 'user@nextmail.com',
     password: '123456',
   },
+  {
+    id: '410544b2-4001-4271-9855-fec4b6123456',
+    name: 'admin',
+    email: 'admin@nextmail.com',
+    password: '123456',
+  },
 ];
 
 const customers = [
