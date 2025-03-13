14634063ac13aa61f77dc947d0ecb8027e0d2bac
zhanglei <01439283>
2025-03-13 15:58:51 +0800

    添加mutaging数据

---
+++ app/lib/action.ts
@@ -0,0 +1,26 @@
+'use server';
+import { z } from 'zod';
+
+
+export async function createInvoice(formData: FormData) {
+    const rawFormData = {
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    };
+    // Test it out:
+    console.log(rawFormData);
+}
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce
+        .number()
+        .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.',
+    }),
+    date: z.string(),
+});
